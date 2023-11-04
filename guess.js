ad2de74c4ba54da7547ef319781f7685db1279a2
BruceHill <brucehillwalley@gmail.com>
2023-11-04 12:58:21 +0300

    enter tuşu eklendi

--- guess.js
+++ guess.js
@@ -6,10 +6,11 @@ console.log(rastgeleSayi);
 
 let mesaj = document.querySelector(".msg");
 
-let skor = 10;
-
+let skor = 10 ;
+//skor u index.html den çekebilirdik. Ama çok kullanacağımız için bu daha tercih edilen yol.
 let enYuksekSkor = 0;
 
+//? Her Check butonuna basıldığında yapılacaklar
 document.querySelector(".check").addEventListener("click", () => {
   const tahmin = document.querySelector(".guess").value;
 
@@ -20,9 +21,60 @@ document.querySelector(".check").addEventListener("click", () => {
   } else if (tahmin == rastgeleSayi) {
     mesaj.textContent = "Tebrikler Bildiniz💪";
     document.querySelector("body").style.backgroundColor="green"
+    document.querySelector(".number").textContent=rastgeleSayi
 
+    // top Score kontrolü
+    if (skor>enYuksekSkor) {
+        enYuksekSkor=skor
+        document.querySelector(".top-score").textContent=enYuksekSkor
+    }
+
+
+    //! tahmin yanlışsa
   } else{
-    mesaj.textContent="Bilemediniz😢"
     
+
+    //? skor 1'den büyük olduğu sürece tahmin hakkım var
+    if (skor>1) {
+        skor--;
+        document.querySelector(".score").textContent=skor
+
+        tahmin < rastgeleSayi?mesaj.textContent="Arttır👆"
+        :mesaj.textContent="Azalt👇"
+    }else{
+        mesaj.textContent="Oyunu Kaybettiniz😢"
+        document.querySelector(".score").textContent=0
+        document.querySelector("body").style.backgroundColor="red"
+
+        //!oyunu kaybettiniz
+    }
   }
 });
+
+//
+
+document.querySelector(".again").onclick=()=>{
+    document.querySelector("body").style.backgroundColor="#2d3436"
+
+    rastgeleSayi=Math.ceil(Math.random()*20) 
+    console.log(rastgeleSayi);
+    skor=10
+
+    document.querySelector(".score").textContent=skor
+
+    document.querySelector(".number").textContent="?"
+
+    document.querySelector(".guess").value=""
+
+    mesaj.textContent="Oyun Yeni Oyuncu İçin Başlıyor5"
+}
+
+//! ENTER
+//* kLAVYEDEN ENTERE BASILDIĞINDA CHECK BUTONU TETİKLENSİN
+
+document.addEventListener("keydown", function(e){
+    if(e.key==="Enter"){
+        //* tuşa basıldı
+        document.querySelector(".check").click()
+    }
+})
